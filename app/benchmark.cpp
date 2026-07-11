f323f1fe63556b0adc826bb9f8ee78acbe7fa18f
Pansho <franciscoromero721@gmail.com>
2026-07-11 15:49:54 -0400

    FEAT: Added benchmark for Graph with bitVectors

--- app/benchmark.cpp
+++ app/benchmark.cpp
@@ -5,6 +5,8 @@
 #include "graph.h"
 #include "graphBitVector.h"
 
+int MAX = 999999999; // Placeholder for unimplemented functions
+
 
 BenchmarkDoc General(graph g){
     BenchmarkDoc doc(g.getName(), 0.0, 0.0, 0.0, 0.0, 0.0);
@@ -13,18 +15,52 @@ BenchmarkDoc General(graph g){
 }
 
 
-BenchmarkDoc runGraphBitVectorBenchmark(){
+BenchmarkDoc runGraphBitVectorBenchmark(std::string file){
     std::cout << "GraphBitVector Benchmark" << std::endl;
     BenchmarkDoc doc("GraphBitVector", 0.0, 0.0, 0.0, 0.0, 0.0);
+
+    // Construction of the GraphBitVector
     auto initConstructionTime = std::chrono::high_resolution_clock::now();
 
-    GraphBitVector graphBitVector("reddit_hyperlinks_body.txt", N, E);
-
+    GraphBitVector graphBitVector(file, N, E);
+    
     auto endConstructionTime = std::chrono::high_resolution_clock::now();
     std::chrono::duration<float> constructionDuration = endConstructionTime - initConstructionTime;
     doc.ConstructionTime = constructionDuration.count();
 
-    
+
+
+    // Benchmark adjacency check
+    auto initAdjTime = std::chrono::high_resolution_clock::now();
+
+    graphBitVector.adj(0, 1);
+
+    auto endAdjTime = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<float> adjDuration = endAdjTime - initAdjTime;
+    doc.adjTime = adjDuration.count();
+
+
+    // Benchmark neighbor
+    auto initNeighTime = std::chrono::high_resolution_clock::now();
+
+    graphBitVector.neigh(0, 1);
+
+    auto endNeighTime = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<float> neighDuration = endNeighTime - initNeighTime;
+    doc.rneighTime = neighDuration.count();
+
+    // Benchmark outDegree
+    auto initOutDegreeTime = std::chrono::high_resolution_clock::now();
+
+    graphBitVector.outDegree(0);
+
+    auto endOutDegreeTime = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<float> outDegreeDuration = endOutDegreeTime - initOutDegreeTime;
+    doc.outDegreeTime = outDegreeDuration.count();
+
+    // Benchmark inDegree and reverse neighbor
+    doc.inDegreeTime = MAX; // Not implemented in GraphBitVector
+    doc.rneighTime = MAX; // Not implemented in GraphBitVector
 
     return doc;
 }
@@ -51,11 +87,16 @@ BenchmarkDoc runClusteredK2TreeBenchmark(){
 }
 
 int main(int argc, char* argv[]){
+    if (argc < 1){
+        std::cerr << "Usage: " << argv[0] << " <graph_file>" << std::endl;
+        return 1;
+    }
+    std::string file = argv[1];
 
     std::cout << "/// BENCHMARK START ///" << std::endl;
 
     std::cout << "-- Graph Bitvector --" << std::endl;
-    BenchmarkDoc gbv = runGraphBitVectorBenchmark();
+    BenchmarkDoc gbv = runGraphBitVectorBenchmark(file);
 
     std::cout << "-- Graph Sequence --" << std::endl;
     BenchmarkDoc gs = runGraphSequenceBenchmark();
